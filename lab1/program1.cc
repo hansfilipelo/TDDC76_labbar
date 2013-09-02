a76887737e946c81877dc5c58efc135adf9b73e2
U-LT01\fille <fille@lt01.(none)>
2013-09-02 14:34:18 +0200

    Program 1 och 2

---
+++ lab1/program1.cc
@@ -0,0 +1,12 @@
+/*
+* Program som skriver ett meddelande på standard utmatningsfil.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "C++ is so cool!" << endl;
+	return 0;
+}
