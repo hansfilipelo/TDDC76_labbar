3da93e3a97ddbc64bec86853e417f68dcd795f23
hansfilipelo <hansfilipelo@gmail.com>
2013-09-02 14:44:34 +0200

    pg2

---
+++ lab1/program2.cc
@@ -0,0 +1,12 @@
+// Program som skriver ett meddelande på standard utmatningsfil.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string message{"C++ is so cool!"};
+	cout << "\n" << message << "\n\n";
+	return 0;
+}
