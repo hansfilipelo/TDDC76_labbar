708e55e6aa0984ddcf87c16ffa7e6c5459377b00
Erik <eriek984@student.liu.se>
2013-09-04 15:30:34 +0200

    lab1-2 complete!

---
+++ lab1/lab1-2.cc
@@ -0,0 +1,32 @@
+// lab1-2.cc
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+
+int main () {
+	
+	double kelvin = 1; 
+	
+	while ( kelvin != 0) {
+		cout << "Ge en temperatur i Kelvin:" << "\n";
+		cin >> kelvin;
+		
+		if ( kelvin < 0 ) {
+			cout << "Negativa kelvinvärden är ej tillåtna!" << "\n\n";
+		}
+		
+		else {
+			double celsius = kelvin - 273.15;
+			double fahrenheit = 1.8*kelvin - 495.67;
+			cout << kelvin << " Kelvin motsvarar " << celsius << " grader Celsius eller " << fahrenheit << " grader Fahrenheit." << "\n\n";
+		}
+	}
+	
+	cout << "Slut" << "\n";
+	
+	return 0;
+}
