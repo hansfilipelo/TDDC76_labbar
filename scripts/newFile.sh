e8a67df681974924dc55a89b21cd30d2eb815da9
Hans-Filip Elo <hansfilipelo@gmail.com>
2013-09-22 23:42:13 +0200

    newFile.sh

--- scripts/newFile.sh
+++ scripts/newFile.sh
@@ -9,3 +9,10 @@ echo "PROGRAMMERARE:	hanel742, eriek984" >> $1
 echo "DATUM:			$(date '+%Y-%m-%d')" >> $1
 echo "BESKRIVNING:	Laboration 1-5" >> $1
 echo "*/" >> $1
+echo "" >> $1
+echo "#include <stdlib.h>" >> $1
+echo "#include <iostream>" >> $1
+echo "#include <iomanip>" >> $1
+echo "#include <fstream>" >> $1
+echo "" >> $1
+echo "using namespace std;" >> $1
