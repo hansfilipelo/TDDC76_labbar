011d9fd784e26efcf29a246ccb65dc990d4bd43a
Hans-Filip Elo <hansfilipelo@gmail.com>
2013-09-22 23:26:26 +0200

    Scripts for initiating work and new files.

---
+++ scripts/newFile.sh
@@ -0,0 +1,13 @@
+#!/bin/tcsh
+
+FILE=$1
+
+touch $FILE
+
+echo "/*" >> $FILE
+echo "FILNAMN: 		Lab1-5.cc" >> $FILE
+echo "LABORATION:		1-5" >> $FILE
+echo "PROGRAMMERARE:	hanel742, eriek984" >> $FILE
+echo "DATUM:			$(date '+%Y-%m-%d')" >> $FILE
+echo "BESKRIVNING:	Laboration 1-5" >> $FILE
+echo "*/" >> $FILE
---
+++ scripts/setupTDDC76.sh
@@ -0,0 +1,6 @@
+#!/bin/tcsh
+
+# Add some of them modules
+module rm prog/gcc
+module add prog/gcc/4
+module add prog/git
