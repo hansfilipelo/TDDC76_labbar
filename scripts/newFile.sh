d0cc0dd35ecf108554f06c7a012e16e0d4fd8c3b
Hans-Filip Elo <hansfilipelo@gmail.com>
2013-09-23 11:25:21 +0200

    newFile.sh

--- scripts/newFile.sh
+++ scripts/newFile.sh
@@ -1,18 +1,28 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-touch $1
+FILE=$1
+touch $FILE
 
-echo "/*" >> $1
-echo "FILNAMN: 		$1" >> $1
-echo "LABORATION:		1-5" >> $1
-echo "PROGRAMMERARE:	hanel742, eriek984" >> $1
-echo "DATUM:			$(date '+%Y-%m-%d')" >> $1
-echo "BESKRIVNING:	Laboration 1-5" >> $1
-echo "*/" >> $1
-echo "" >> $1
-echo "#include <stdlib.h>" >> $1
-echo "#include <iostream>" >> $1
-echo "#include <iomanip>" >> $1
-echo "#include <fstream>" >> $1
-echo "" >> $1
-echo "using namespace std;" >> $1
+cat << _EOF_ > ./$FILE
+/*
+_EOF_
+
+echo "FILNAMN: 		$FILE" >> $FILE
+
+cat << _EOF_ >> ./$FILE
+LABORATION:		
+PROGRAMMERARE:	hanel742, eriek984
+_EOF_
+
+echo "DATUM:			$(date '+%Y-%m-%d')" >> $FILE
+
+cat << _EOF_ >> ./$FILE
+BESKRIVNING:	Laboration 1-5
+*/
+
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+_EOF_
