1d32ff27a8647f1cf5b465125e2601e9b754892d
Erik <eriek984@student.liu.se>
2013-09-26 08:49:06 +0200

    nya filer

---
+++ lab2/Monetary.cc
@@ -0,0 +1,18 @@
+/*
+FILNAMN: 		Monetary.cc
+LABORATION:		
+PROGRAMMERARE:	hanel742, eriek984
+DATUM:			2013-09-26
+BESKRIVNING:	
+*/
+
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+
+int main(){
+	
+	return 0;
+}
