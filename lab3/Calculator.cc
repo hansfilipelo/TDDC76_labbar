0d176a4a0fa6384e34aa8c42ee6458d709db9f7e
Erik <eriek984@student.liu.se>
2013-10-09 13:18:09 +0200

    Added files

---
+++ lab3/Calculator.cc
@@ -0,0 +1,19 @@
+/*
+FILNAMN: 		Calculator.cc
+LABORATION:		
+PROGRAMMERARE:	hanel742, eriek984
+DATUM:			2013-10-09
+BESKRIVNING:	
+*/
+
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+	
+	return 0;
+}
