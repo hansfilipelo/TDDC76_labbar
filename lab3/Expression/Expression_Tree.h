1e62dcd3da2deb9054668c75ff808b409fe1bb76
Rageface <eriek984@student.liu.se>
2013-10-09 13:42:54 +0200

    Jobbar XCode ist...

--- lab3/Expression/Expression_Tree.h
+++ lab3/Expression/Expression_Tree.h
@@ -61,9 +61,23 @@ class Power: public Binary_Operator
 {
 };
 
+//--------------------------------
+// Integer for representing integer values
+
 class Integer : public Operand
 {
+	public:
+		Integer(int inValue);
+		long double      evaluate() const = 0;
+		std::string      get_postfix() const = 0;
+		std::string      str() const = 0;
+		void             print(std::ostream&) const = 0;
+		Expression_Tree* clone() const = 0;
+	private:
+		int value;
 };
+
+//--------------------------------
 
 class Real : public Operand
 {
